48ce5c1f143436a6780d69a668334e1a8981657d
Jino Park <pjessesco@gmail.com>
2021-07-15 14:13:47 +0900

    Rename variables and add `quit()` action

--- gui.h
+++ gui.h
@@ -30,34 +30,38 @@ public:
 
         /* After that create a context menu of two items */
         QMenu *menu = new QMenu(this);
-        QAction *update_wallpaper = new QAction("Update background", this);
-        QAction *color_rgb_true = new QAction("RGB True");
-        QAction *color_natural = new QAction("Natural");
+        QAction *update_wallpaper_action = new QAction("Update background", this);
+        QAction *color_rgb_true_action = new QAction("RGB True");
+        QAction *color_natural_action = new QAction("Natural");
+        QAction *quit_action = new QAction("Quit");
 
-        menu->addAction(update_wallpaper);
+        menu->addAction(update_wallpaper_action);
         menu->addSection("Colors");
-        menu->addAction(color_rgb_true);
-        menu->addAction(color_natural);
+        menu->addAction(color_rgb_true_action);
+        menu->addAction(color_natural_action);
+        menu->addSeparator();
+        menu->addAction(quit_action);
 
         QActionGroup *set_color_group = new QActionGroup(this);
         set_color_group->setExclusive(true);
-        set_color_group->addAction(color_rgb_true);
-        set_color_group->addAction(color_natural);
-        color_rgb_true->setCheckable(true);
-        color_rgb_true->setChecked(true);
-        color_natural->setCheckable(true);
+        set_color_group->addAction(color_rgb_true_action);
+        set_color_group->addAction(color_natural_action);
+        color_rgb_true_action->setCheckable(true);
+        color_rgb_true_action->setChecked(true);
+        color_natural_action->setCheckable(true);
 
-        connect(update_wallpaper, SIGNAL(triggered()), this, SLOT(change_wallpaper()));
-        connect(color_rgb_true, SIGNAL(triggered()), this, SLOT(set_color_rgb_true()));
-        connect(color_natural, SIGNAL(triggered()), this, SLOT(set_color_natural()));
-        
+        connect(update_wallpaper_action, SIGNAL(triggered()), this, SLOT(change_wallpaper_slot()));
+        connect(color_rgb_true_action, SIGNAL(triggered()), this, SLOT(set_color_rgb_true_slot()));
+        connect(color_natural_action, SIGNAL(triggered()), this, SLOT(set_color_natural_slot()));
+        connect(quit_action, SIGNAL(triggered()), this, SLOT(quit_slot()));
+
         trayIcon->setContextMenu(menu);
         trayIcon->show();
         trayIcon->setIcon(QIcon("../icon.png"));
     };
 
 public slots:
-    void change_wallpaper(){
+    void change_wallpaper_slot(){
         std::cout<<"change wallpaper!\n";
         boost::posix_time::ptime utcTime = boost::posix_time::second_clock::universal_time();
         auto adjusted_time = adjust_target_time(utcTime);
@@ -69,29 +73,21 @@ public slots:
         std::cout<<"done!\n";
     };
 
-    void set_color_rgb_true(){
+    void set_color_rgb_true_slot(){
         m_color = Color::True;
     }
 
-    void set_color_natural(){
+    void set_color_natural_slot(){
         m_color = Color::Natural;
     }
 
-
-
-    void closeEvent(QCloseEvent *event){
-#ifdef Q_OS_MACOS
-        if (!event->spontaneous() || !isVisible()) {
-            return;
-        }
-#endif
-        if (trayIcon->isVisible()) {
-            hide();
-            event->ignore();
-        }
+    void quit_slot(){
+        QCoreApplication::quit();
     }
 
+
 private:
+
     QSystemTrayIcon *trayIcon;
     ImageType m_imgType;
     Color m_color;
