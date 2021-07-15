5da8ee8b8d263ee89553998e842b98238096f1ac
Jino Park <pjessesco@gmail.com>
2021-07-15 17:01:43 +0900

    Move source codes into `src` directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-#include <tuple>
-
-#include <boost/date_time.hpp>
-
 #include <QApplication>
-
 #include "gui.h"
-#include "image.h"
 
 int main(int argc, char *argv[]) {
 
-
     QApplication app(argc, argv);
+    app.setQuitOnLastWindowClosed(false);
     GUI gui;
 
     return app.exec();
