b6b0a306fdd23ce4b1d99d95b23f118448c96f18
Jino Park <pjessesco@gmail.com>
2021-09-15 15:36:44 +0900

    Copy `dbus.dylib` from homebrew, and change related paths

--- scripts/bundle_macos.py
+++ scripts/bundle_macos.py
@@ -63,8 +63,10 @@ if __name__ == "__main__":
 
     # Copy qt dependency using macdeployqt
     os.system("macdeployqt " + APP_NAME_IN_CMD)
-    shutil.copytree(os.path.join(QT_FRAMEWORK_PATH, "QtDBus.framework"), os.path.join(APP_NAME, "Contents", "Frameworks", "QtDBus.framework"), symlinks=True)
 
+    # Copy other dependency manually (qtdbus, libdbus)
+    shutil.copytree(os.path.join(QT_FRAMEWORK_PATH, "QtDBus.framework"), os.path.join(APP_NAME, "Contents", "Frameworks", "QtDBus.framework"), symlinks=True)
+    shutil.copy("/usr/local/opt/dbus/lib/libdbus-1.3.dylib", os.path.join(APP_NAME, "Contents", "Frameworks", "libdbus-1.3.dylib"))
 
     # Fix dependency path contains @rpath manually
 
@@ -72,17 +74,25 @@ if __name__ == "__main__":
     QT_GUI_FRAMEWORK_IN_BUNDLE = os.path.join(APP_NAME_IN_CMD, "Contents", "Frameworks", "QtGui.framework", "QtGui")
     QT_WIDGETS_FRAMEWORK_IN_BUNDLE = os.path.join(APP_NAME_IN_CMD, "Contents", "Frameworks", "QtWidgets.framework", "QtWidgets")
 
+    # Fix id of embedded libdbus library
+    os.system("install_name_tool -id @executable_path/../Frameworks/libdbus-1.3.dylib " + os.path.join(APP_NAME_IN_CMD, "Contents", "Frameworks", "libdbus-1.3.dylib"))
+
+    # Fix id & dependency of embedded QtDbus framework
+    os.system("install_name_tool -id @executable_path/../Frameworks/QtDBus.framework/Versions/A/QtDBus " + QT_DBUS_FRAMEWORK_IN_BUNDLE)
     os.system("install_name_tool -change @rpath/QtCore.framework/Versions/A/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/A/QtCore " + QT_DBUS_FRAMEWORK_IN_BUNDLE)
 
+    os.system("install_name_tool -change /usr/local/opt/dbus/lib/libdbus-1.3.dylib @executable_path/../Frameworks/libdbus-1.3.dylib " + QT_DBUS_FRAMEWORK_IN_BUNDLE)
+
+    # Fix dependency of embedded QtGui framework
     os.system("install_name_tool -change @rpath/QtDBus.framework/Versions/A/QtDBus @executable_path/../Frameworks/QtDBus.framework/Versions/A/QtDBus " + QT_GUI_FRAMEWORK_IN_BUNDLE)
     os.system("install_name_tool -change @rpath/QtCore.framework/Versions/A/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/A/QtCore " + QT_GUI_FRAMEWORK_IN_BUNDLE)
 
+    # Fix dependency of embedded QtWidgets framework
     os.system("install_name_tool -change @rpath/QtGui.framework/Versions/A/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/A/QtGui " + QT_WIDGETS_FRAMEWORK_IN_BUNDLE)
     os.system("install_name_tool -change @rpath/QtCore.framework/Versions/A/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/A/QtCore " + QT_WIDGETS_FRAMEWORK_IN_BUNDLE)
 
     # Copy resources (icon)
     shutil.copy(os.path.join("..", "resources", "icon.png"), os.path.join(APP_NAME, "Contents", "Resources", "icon.png"))
-
 
     print("**********************************************")
     print("*                                            *")
