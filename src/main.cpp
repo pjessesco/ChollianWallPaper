03480179f7c050638b03789deb93d64de2fb03da
Jino Park <pjessesco@gmail.com>
2021-10-24 15:43:21 +0900

    Hide cmd console in Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <filesystem>
+#if defined _WIN32
+    #include <windows.h>
+#endif
 
 #include <QApplication>
 
 #include "chollian.h"
 
 int main(int argc, char *argv[]) {
+    
+#if defined _WIN32
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
+
     std::filesystem::current_path(std::filesystem::path(argv[0]).parent_path());
 
     QApplication app(argc, argv);
